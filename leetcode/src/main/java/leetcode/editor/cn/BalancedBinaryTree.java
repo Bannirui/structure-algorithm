cdae92b3f40b19460c21fd41d5f32e232f44b5d0
Bannirui <1652959004@qq.com>
2021-06-09 12:30:55 +0800

    add: [110]平衡二叉树

--- leetcode/src/main/java/leetcode/editor/cn/BalancedBinaryTree.java
+++ leetcode/src/main/java/leetcode/editor/cn/BalancedBinaryTree.java
@@ -1,73 +1,56 @@
 package leetcode.editor.cn;
 
-//给定一个二叉树，判断它是否是高度平衡的二叉树。 
+//给定一个二叉树，判断它是否是高度平衡的二叉树。
 //
-// 本题中，一棵高度平衡二叉树定义为： 
+// 本题中，一棵高度平衡二叉树定义为：
 //
-// 
-// 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。 
-// 
 //
-// 
+// 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。
 //
-// 示例 1： 
 //
-// 
+//
+//
+// 示例 1：
+//
+//
 //输入：root = [3,9,20,null,null,15,7]
 //输出：true
-// 
 //
-// 示例 2： 
 //
-// 
+// 示例 2：
+//
+//
 //输入：root = [1,2,2,3,3,null,null,4,4]
 //输出：false
-// 
 //
-// 示例 3： 
 //
-// 
+// 示例 3：
+//
+//
 //输入：root = []
 //输出：true
-// 
 //
-// 
 //
-// 提示： 
 //
-// 
-// 树中的节点数在范围 [0, 5000] 内 
-// -104 <= Node.val <= 104 
-// 
-// Related Topics 树 深度优先搜索 递归 
-// 👍 555 👎 0
+//
+// 提示：
+//
+//
+// 树中的节点数在范围 [0, 5000] 内
+// -104 <= Node.val <= 104
+//
+// Related Topics 树 深度优先搜索 递归
+// 👍 701 👎 0
+
+import java.util.regex.Matcher;
 
 public class BalancedBinaryTree {
     public static void main(String[] args) {
         Solution solution = new BalancedBinaryTree().new Solution();
-
-        // TreeNode node1 = new TreeNode(1);
-        // TreeNode node2 = new TreeNode(2);
-        // TreeNode node3 = new TreeNode(2);
-        // TreeNode node4 = new TreeNode(3);
-        // TreeNode node5 = new TreeNode(3);
-        // TreeNode node6 = new TreeNode(4);
-        // TreeNode node7 = new TreeNode(4);
-
-        // node4.left=node6;
-        // node4.right=node7;
-        // node2.left=node4;
-        // node2.right=node5;
-        // node1.left=node2;
-        // node1.right=node3;
-
-        // boolean balanced = solution.isBalanced(node1);
-        // System.out.println();
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
-
-    // Definition for a binary tree node.
+//Definition for a binary tree node.
     public class TreeNode {
         int val;
         TreeNode left;
@@ -87,59 +70,61 @@ public class BalancedBinaryTree {
         }
     }
 
+    class BalanceTreeNodeInfo {
+
+        // 是否是完全二叉树
+        boolean isAllBalanced;
+
+        // 树高
+        int height;
+
+        public BalanceTreeNodeInfo(boolean isAllBalanced, int height) {
+            this.isAllBalanced = isAllBalanced;
+            this.height = height;
+        }
+
+        public boolean isAllBalanced() {
+            return isAllBalanced;
+        }
+
+        public void setAllBalanced(boolean allBalanced) {
+            isAllBalanced = allBalanced;
+        }
+
+        public int getHeight() {
+            return height;
+        }
+
+        public void setHeight(int height) {
+            this.height = height;
+        }
+    }
+
     class Solution {
         public boolean isBalanced(TreeNode root) {
-            // return Math.abs(treeHeight(root, true) - treeHeight(root, false)) <= 1;
-            if (root == null) {
-                return true;
-            }
-            return Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+            return obtainInfo(root).isAllBalanced();
         }
 
-        public int height(TreeNode root) {
-            // 根节点为空
-            if (root == null) {
-                return 0;
+        public BalanceTreeNodeInfo obtainInfo(TreeNode x) {
+            // base case
+            if (x == null) {
+                return new BalanceTreeNodeInfo(true, 0);
             }
-            // 根节点不为空
-            return Math.max(height(root.left), height(root.right)) + 1;
-        }
-
-        public int treeHeight(TreeNode root, boolean isLongest) {
-            if (isLongest) {
-                // 最长路径
-                // 根节点为空
-                if (root == null) {
-                    return 0;
-                }
-                // 根节点不为空 左右子节点都为空
-                if (root.left == null && root.right == null) {
-                    return 1;
-                }
-                // 根节点不为空 左右子节点可能为空
-                if (root.left == null) {
-                    return treeHeight(root.right, isLongest) + 1;
-                }
-                if (root.right == null) {
-                    return treeHeight(root.left, isLongest) + 1;
-                }
-                // 根节点不为空 左右子节点都不为空
-                return Math.max(treeHeight(root.left, isLongest), treeHeight(root.right, isLongest)) + 1;
-            } else {
-                // 最短路径
-                // 根节点为空
-                if (root == null) {
-                    return 0;
-                }
-                // 根节点不为空 左右子节点只要有一个为空 最短路径都是当前节点1
-                if (root.left == null || root.right == null) {
-                    return 1;
-                }
-                // 根节点不为空 左右子节点都不为空
-                return Math.min(treeHeight(root.left, isLongest), treeHeight(root.right, isLongest)) + 1;
+            // 左右节点信息
+            BalanceTreeNodeInfo leftTreeInfo = obtainInfo(x.left);
+            BalanceTreeNodeInfo rightTreeInfo = obtainInfo(x.right);
+            // 整合自己信息
+            boolean isBalanced = true;
+            int height = Math.max(leftTreeInfo.getHeight(), rightTreeInfo.getHeight()) + 1;
+            if (
+                    !leftTreeInfo.isAllBalanced() ||
+                            !rightTreeInfo.isAllBalanced() ||
+                            Math.abs(leftTreeInfo.getHeight() - rightTreeInfo.getHeight()) > 1
+            ) {
+                isBalanced = false;
             }
+            return new BalanceTreeNodeInfo(isBalanced, height);
         }
-
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
