a0685b6950ca333dfe83c9bfbe2873a665202132
Bannirui <1652959004@qq.com>
2021-06-28 22:03:01 +0800

    [896] 单调数列

--- leetcode/src/main/java/leetcode/editor/cn/896.单调数列.java
+++ leetcode/src/main/java/leetcode/editor/cn/896.单调数列.java
@@ -1,11 +1,4 @@
 /*
- * @Author: dingrui
- * @Date: 2021-06-28 21:52:01
- * @LastEditors: dingrui
- * @LastEditTime: 2021-06-28 21:58:10
- * @Descripttion: 
- */
-/*
  * @lc app=leetcode.cn id=896 lang=java
  *
  * [896] 单调数列
@@ -14,19 +7,18 @@
 // @lc code=start
 class Solution {
     public boolean isMonotonic(int[] nums) {
-        boolean inc = false, desc = false;
+        boolean asc = false, desc = false;
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] > nums[i - 1]) {
                 // 递增
-                inc = true;
+                asc = true;
             }
             if (nums[i] < nums[i - 1]) {
                 // 递减
                 desc = true;
             }
         }
-        // 同时存在 递增段和递减段
-        return !(inc && desc);
+        return !(asc & desc);
     }
 }
 // @lc code=end
