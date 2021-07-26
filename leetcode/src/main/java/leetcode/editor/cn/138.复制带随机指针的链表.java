616cd1acf43e0bbae1422b5ff3c621945462e6e3
Bannirui <1652959004@qq.com>
2021-07-26 14:03:39 +0800

    [138] 复制带随机指针的链表

---
+++ leetcode/src/main/java/leetcode/editor/cn/138.复制带随机指针的链表.java
@@ -0,0 +1,52 @@
+/*
+ * @Author: dingrui
+ * @Date: 2021-07-26 13:46:11
+ * @LastEditors: dingrui
+ * @LastEditTime: 2021-07-26 14:02:51
+ * @Descripttion: 
+ */
+/*
+ * @lc app=leetcode.cn id=138 lang=java
+ *
+ * [138] 复制带随机指针的链表
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+        // 复制节点
+        Map<Node, Node> map = new HashMap<>();
+        Node cur = head;
+        while (cur != null) {
+            map.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+        // 遍历 设置random
+        cur = head;
+        while (cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+        return map.get(head);
+    }
+}
+// @lc code=end
