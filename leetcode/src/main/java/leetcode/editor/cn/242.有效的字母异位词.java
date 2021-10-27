61d766f5c692e31dfc700d5ba8c6faa75613e569
bannirui <1652959004@qq.com>
2021-10-27 10:18:16 +0800

    [242] 有效的字母异位词

---
+++ leetcode/src/main/java/leetcode/editor/cn/242.有效的字母异位词.java
@@ -0,0 +1,21 @@
+import java.lang.annotation.Target;
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode.cn id=242 lang=java
+ *
+ * [242] 有效的字母异位词
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        // 异位词的字典序一样
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+        return Arrays.equals(sArr, tArr);
+    }
+}
+// @lc code=end
