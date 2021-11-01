90780cd56788a6676dd3e5bcda44a8c9a3674cb0
bannirui <1652959004@qq.com>
2021-11-01 11:11:22 +0800

    [324] 摆动排序II

---
+++ leetcode/src/main/java/leetcode/editor/cn/324.摆动排序-ii.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=324 lang=java
+ *
+ * [324] 摆动排序 II
+ */
+
+// @lc code=start
+class Solution {
+    public void wiggleSort(int[] nums) {
+        int[] copyNums = nums.clone();
+        Arrays.sort(copyNums);
+        int len = nums.length;
+        for (int i = 1; i < nums.length; i += 2) {
+            nums[i] = copyNums[--len];
+        }
+        for (int i = 0; i < nums.length; i += 2) {
+            nums[i] = copyNums[--len];
+        }
+    }
+}
+// @lc code=end
