cf82709ac1d37d3282a2b24de6f5cc53f1e2e4eb
bannirui <1652959004@qq.com>
2021-11-03 13:48:12 +0800

    [102] 二叉树的层序遍历

---
+++ leetcode/src/main/java/leetcode/editor/cn/102.二叉树的层序遍历.java
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=102 lang=java
+ *
+ * [102] 二叉树的层序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        // 结果集
+        List<List<Integer>> res = new ArrayList<>();
+        if(root==null){
+            return res;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            List<Integer> levelItem = new ArrayList<>();
+            for(int i=0;i<size;i++){
+                TreeNode cur = q.poll();
+                levelItem.add(cur.val);
+                if(cur.left!=null){
+                    q.offer(cur.left);
+                }
+                if(cur.right!=null){
+                    q.offer(cur.right);
+                }
+            }
+            res.add(levelItem);
+        }
+        return res;
+    }
+}
+// @lc code=end
+
