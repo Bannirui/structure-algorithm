fcd6b83844d5063db4270c5a40a5933eb77bcc81
bannirui <1652959004@qq.com>
2021-11-08 10:47:01 +0800

    [236] 二叉树的最近公共祖先

---
+++ leetcode/src/main/java/leetcode/editor/cn/236.二叉树的最近公共祖先.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=236 lang=java
+ *
+ * [236] 二叉树的最近公共祖先
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // base case
+        if(root==null){
+            return root;
+        }
+        if(p==root||q==root){
+            return root;
+        }
+        TreeNode left = this.lowestCommonAncestor(root.left, p, q);
+        TreeNode right = this.lowestCommonAncestor(root.right, p, q);
+        if(left==null&&right==null){
+            return null;
+        }
+        if(left==null){
+            return right;
+        }
+        if(right==null){
+            return left;
+        }
+        return root;
+    }
+}
+// @lc code=end
+
