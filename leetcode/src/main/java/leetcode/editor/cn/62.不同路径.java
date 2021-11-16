378faa274a15ffea4522109c0d5eda1e0a76a816
bannirui <1652959004@qq.com>
2021-11-16 10:34:36 +0800

    [62] 不同路径

---
+++ leetcode/src/main/java/leetcode/editor/cn/62.不同路径.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=62 lang=java
+ *
+ * [62] 不同路径
+ */
+
+// @lc code=start
+class Solution {
+    public int uniquePaths(int m, int n) {
+        // dp[i][j] [i.j]->[m,n]
+        int[][] dp = new int[m][n];
+        // base case
+        for(int i=0;i<n;i++){
+            dp[m-1][i]=1;
+        }
+        for(int i=0;i<m;i++){
+            dp[i][n-1]=1;
+        }
+        // 右下角往左上角推
+        for(int i=m-2;i>=0;i--){
+            for(int j=n-2;j>=0;j--){
+                dp[i][j] = dp[i+1][j]+dp[i][j+1];
+            }
+        }
+        return dp[0][0];
+    }
+}
+// @lc code=end
+
