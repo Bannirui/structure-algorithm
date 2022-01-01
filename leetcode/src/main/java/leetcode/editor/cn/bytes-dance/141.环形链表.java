f06b6159b7e64579406470cb09de593ef011e452
bannirui <1652959004@qq.com>
2022-01-01 11:34:07 +0800

    [141] 环形链表

---
+++ leetcode/src/main/java/leetcode/editor/cn/bytes-dance/141.环形链表.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=java
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head==null || head.next==null || head.next.next==null) return false; // corner condition
+        // double ptr
+        ListNode slow=head.next;
+        ListNode fast=head.next.next;
+        while(slow!=fast){
+            if(fast==null || fast.next==null || fast.next.next==null) return false;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
+// @lc code=end
+
