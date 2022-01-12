0a22174641b2fa4dc0fd0ea2d2f0fe961cafecdf
bannirui <1652959004@qq.com>
2022-01-12 22:41:59 +0800

    [342] 4的幂

---
+++ leetcode/src/main/java/leetcode/editor/cn/bytes-dance/342.4-的幂.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=342 lang=java
+ *
+ * [342] 4的幂
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        return n>0 && (n&(n-1))==0 && n%3==1;
+    }
+}
+// @lc code=end
+
