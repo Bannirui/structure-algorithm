0224a2f178e7a6216e373c55a6b07827a8e7760c
bannirui <1652959004@qq.com>
2022-02-07 14:24:57 +0800

    [117] 填充每个节点的下一个右侧节点指针

---
+++ leetcode/src/main/java/leetcode/editor/cn/bytes-dance/117.填充每个节点的下一个右侧节点指针-ii.java
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=117 lang=java
+ *
+ * [117] 填充每个节点的下一个右侧节点指针 II
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if(root==null) return null;
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            Node pre = null;
+            for(int i=0;i<size;i++){
+                Node cur = q.poll();
+                if(cur.left!=null) q.offer(cur.left);
+                if(cur.right!=null) q.offer(cur.right);
+                if(i!=0) pre.next=cur; 
+                pre=cur;
+            }
+        }
+        return root;
+    }
+}
+// @lc code=end
+
