2842dbaf9b495299d8027f1b300652e318bdc528
dingrui <1652959004@qq.com>
2022-06-25 22:46:18 +0800

    [200] 岛屿数量

--- leetcode/src/main/java/leetcode/editor/cn/bytes-dance/200.岛屿数量.java
+++ leetcode/src/main/java/leetcode/editor/cn/bytes-dance/200.岛屿数量.java
@@ -10,33 +10,41 @@ class Solution {
     private int[][] directions = new int[][]{{-1,0},{1,0},{0,-1},{0,1}};
 
     public int numIslands(char[][] grid) {
+        int ROWS=0, COLS=0;
+        if(grid==null || (ROWS=grid.length)==0 || (COLS=grid[0].length)==0) throw new IllegalArgumentException();
         int cnt = 0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
+        for(int i=0;i<ROWS;i++){
+            for(int j=0;j<COLS;j++){
                 if(grid[i][j]=='1'){
-                    // cnt
                     cnt++;
                     this.infect(grid, i, j);
                 }
             }
         }
+        // recover
+        this.recover(grid);
         return cnt;
     }
 
     // 1->2
-    private void infect(char[][] grid, int x, int y){
+    private void infect(char[][] grid, int x, int y)
+    {
         // base case
-        if(x<0||x>=grid.length||y<0||y>=grid[0].length||grid[x][y]!='1'){
-            return;
-        }
+        if(x<0||x>=grid.length||y<0||y>=grid[0].length||grid[x][y]!='1') return;
         // grid[x][y] 1->2
         grid[x][y]='2';
         // dfs
-        for(int[] direction: directions){
-            int newX = x+direction[0];
-            int newY = y+direction[1];
-            this.infect(grid, newX, newY);
-        }
+        for(int[] direction: directions) this.infect(grid, x+direction[0], y+direction[1]);
+    }
+
+    // 2->1
+    private void recover(char[][] grid)
+    {
+        int ROWS=0, COLS=0;
+        if(grid==null || (ROWS=grid.length)==0 || (COLS=grid[0].length)==0) return;
+        for(int i=0;i<ROWS;i++)
+            for(int j=0;j<COLS;j++)
+                if(grid[i][j]=='2') grid[i][j]='1';
     }
 }
 // @lc code=end
