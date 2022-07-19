5b2122d51de4b16e09a0ced99e86d244b708cba6
dingrui <1652959004@qq.com>
2022-07-19 17:14:39 +0800

    [123] 买卖股票的最佳时机III

---
+++ leetcode/src/main/java/leetcode/editor/cn/bytes-dance/123.买卖股票的最佳时机-iii.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=123 lang=java
+ *
+ * [123] 买卖股票的最佳时机 III
+ */
+
+// @lc code=start
+class Solution {
+    public int maxProfit(int[] prices) {
+        int len=0;
+        if(prices==null || (len=prices.length)==0) return 0;
+        int buy1=-prices[0];
+        int sell1=0;
+        int buy2=-prices[0];
+        int sell2=0;
+        for(int i=1;i<len;i++){
+            buy1=Math.max(buy1,-prices[i]);
+            sell1=Math.max(sell1,buy1+prices[i]);
+            buy2=Math.max(buy2,sell1-prices[i]);
+            sell2=Math.max(sell2,buy2+prices[i]);
+        }
+        return sell2;
+    }
+}
+// @lc code=end
+
